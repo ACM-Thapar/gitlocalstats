396fb028d9f25a7060c8b8a0bb11b0ef2336c73e
Amit Goyal <agoyal3_be21@thapar.edu>
2022-10-29 14:15:55 +0530

    Scanning files successful

--- main.go
+++ main.go
@@ -2,61 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"os"
-	"strings"
 )
-
-func scan(folder string) {
-	fmt.Printf("Found folders:\n\n")
-	repositories := recursiveScanFolder(folder)
-	filepath := getDotFilePath()
-	addNewSliceElementsToFile(filepath, repositories)
-	fmt.Printf("\n\nSuccessfully added\n\n")
-}
-
-func stats(email string) {
-	print("stats")
-}
-
-func recursiveScanFolder(folder string) []string {
-	return scanGitFolders(make([]string, 0), folder)
-}
-
-func scanGitFolders(folders []string, folder string) []string {
-	folder = strings.TrimSuffix(folder, "/")
-
-	f, err := os.Open(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-	files, err := f.ReadDir(-1)
-	f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var path string
-
-	for _, file := range files {
-		if file.IsDir() {
-			path = folder + "/" + file.Name()
-			if file.Name() == ".git" {
-				path = strings.TrimSuffix(path, "/.git")
-				fmt.Println(path)
-				folders = append(folders, path)
-				continue
-			}
-
-			if file.Name() == "vendor" || file.Name() == "node_modules" {
-				continue
-			}
-			folders = scanGitFolders(folders, path)
-		}
-	}
-	return folders
-}
 
 func main() {
 	var folder string
---
+++ stats.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/go-git/go-git/plumbing/object"
+	"github.com/go-git/go-git/v5"
+)
+
+func stats(email string) {
+	commits := processRepositories(email)
+	printCommitStats(commits)
+}
+
+func processRepositories(email string) map[int]int {
+	filepath := getDotFilePath()
+	repos := parseFileLinesToSlice(filepath)
+	daysInMap := daysInLastSixMonths
+
+	commits := make(map[int]int, daysInMap)
+	for i := daysInMap; i > 0; i-- {
+		commits[i] = 0
+	}
+
+	for _, path := range repos {
+		commits := fillCommits(email, path, commits)
+	}
+	return commits
+}
+
+func fillCommits(email string, path string, commits map[int]int) map[int]int {
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		panic(err)
+	}
+
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	offset := calcOffset()
+
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := countDaysSinceDate(c.Author.When) + offset
+
+		if c.Author.Email != email {
+			return nil
+		}
+
+		if daysAgo != outOfRange {
+			commits[daysAgo]++
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return commits
+}
