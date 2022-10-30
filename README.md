5bea0554cc5ec73106a8318c1f73f86a151c568e
Amit Goyal <agoyal3_be21@thapar.edu>
2022-10-30 10:23:33 +0530

    processing of commits done

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gitlocalstats
-A CLI command that generates a Github Contributions Graph in your terminal by scanning the .git folders in your local system
+A CLI that generates a Github Contributions Graph in your terminal by scanning the .git folders in your local system
+
+## Usage
+
+Run `go install` to make the command `gitlocalstats` working
+
+- `gitlocalstats -add /path/to/folder` scans that folder and its subdirectories for repos to scan
+- `gitlocalstats -email your@email.com` generates a graph that represents the last 6 month of activity for the passed email.
+
+## Demonstration
--- stats.go
+++ stats.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"time"
+
 	"github.com/go-git/go-git/plumbing/object"
 	"github.com/go-git/go-git/v5"
 )
+
+const daysInLastSixMonths = 183
+const outOfRange = 99999
 
 func stats(email string) {
 	commits := processRepositories(email)
@@ -63,4 +68,47 @@ func fillCommits(email string, path string, commits map[int]int) map[int]int {
 	}
 
 	return commits
+}
+
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return startOfDay
+}
+
+func countDaysSinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+	return days
+}
+
+func calcOffset() int{
+	var offset int
+	weekday := time.Now().Weekday()
+
+	switch weekday{
+		 case time.Sunday:
+        offset = 7
+    case time.Monday:
+        offset = 6
+    case time.Tuesday:
+        offset = 5
+    case time.Wednesday:
+        offset = 4
+    case time.Thursday:
+        offset = 3
+    case time.Friday:
+        offset = 2
+    case time.Saturday:
+        offset = 
+	}
+
+	return offset
 }
